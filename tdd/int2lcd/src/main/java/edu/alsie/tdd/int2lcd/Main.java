3b845c8b83be3f3291e87704400aabe148a364d8
alexis ardaya <alexisardaya@gmail.com>
2018-06-14 23:58:14 -0400

    Added  print numberLcd on client

--- tdd/int2lcd/src/main/java/edu/alsie/tdd/int2lcd/Main.java
+++ tdd/int2lcd/src/main/java/edu/alsie/tdd/int2lcd/Main.java
@@ -1,8 +1,16 @@
 package edu.alsie.tdd.int2lcd;
 
+import java.util.List;
+
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        List<String> numberLcd;
 
-  }
+        Int2Lcd int2Lcd = new Int2Lcd();
+        numberLcd = int2Lcd.convertDigitToLcd(1);
+        for (int i = 0; i < numberLcd.size(); i++) {
+            System.out.println(numberLcd.get(i));
+        }
+    }
 }
