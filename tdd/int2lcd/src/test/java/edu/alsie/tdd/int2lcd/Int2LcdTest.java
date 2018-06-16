f3117b0095d3f149922d41fbd18ca62ea933e876
alexis ardaya <alexisardaya@gmail.com>
2018-06-15 23:03:22 -0400

    Added test to convert number  six to number lcd

--- tdd/int2lcd/src/test/java/edu/alsie/tdd/int2lcd/Int2LcdTest.java
+++ tdd/int2lcd/src/test/java/edu/alsie/tdd/int2lcd/Int2LcdTest.java
@@ -108,4 +108,12 @@ public class Int2LcdTest {
     List<String> actual = int2Lcd.convertDigitToLcd(5);
     Assert.assertEquals(expected, actual);
   }
+
+  @Test
+  public void testGiven6ThenConvertToLcd() {
+    Int2Lcd int2Lcd = new Int2Lcd();
+    List<String> expected = Arrays.asList("_","|_","|_|");
+    List<String> actual = int2Lcd.convertDigitToLcd(6);
+    Assert.assertEquals(expected, actual);
+  }
 }
